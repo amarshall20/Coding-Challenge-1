2909750df4044e1df5da65aac0b09854678c3da3
Alex Marshall <33075034+amarshall20@users.noreply.github.com>
2018-03-27 03:27:23 -0600

    Initial commit

---
+++ CodingChallenge1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodingChallenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "She sells seashells by the seashore";
+            char[] array = input.ToCharArray();
+
+            for (int i = 0; i < (array.Length / 2); i++)
+            {
+                char temp = array[i];
+                array[i] = array[array.Length - 1 - i];
+                array[array.Length - 1 - i] = temp;
+            }
+
+            string reversedString = "";
+            foreach (char character in array)
+            {
+                reversedString += character;
+            }
+            Console.WriteLine(reversedString);
+            Console.ReadLine();
+        }
+    }
+}
